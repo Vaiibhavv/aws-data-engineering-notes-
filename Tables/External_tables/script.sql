8034c5fe3ad2d6ac54fc01ca170f5f9d8e23b641
vaibhav <vaibhavsaner@gmail.com>
2026-06-19 01:23:09 +0530

    external_table modified

--- Tables/External_tables/script.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS expensive
-  WITH ( format='parquet', external_location='s3://vp-bucket-pract/destination_for_external_table/') AS
-SELECT
-  *
-FROM
-  orders
-WHERE
-  item='Monitor';
